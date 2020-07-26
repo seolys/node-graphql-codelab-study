6f228f3d1e53abe009935a9f634d270675c69107
seol <>
2020-07-26 19:09:53 +0900

    dummy db

---
+++ src/db/Article.js
@@ -0,0 +1,46 @@
+module.exports = [
+    {
+        id: 1,
+        createdAt: "",
+        updatedAt: "",
+        category: 1,
+        title: "안녕하세요11",
+        viewNum: 12,
+        content: "하이하이11",
+        author: 1,
+        thumbnail: 3
+    },
+    {
+        id: 2,
+        createdAt: "",
+        updatedAt: "",
+        category: 1,
+        title: "안녕하세요22",
+        viewNum: 12,
+        content: "하이하이22",
+        author: 1,
+        thumbnail: 3
+    },
+    {
+        id: 3,
+        createdAt: "",
+        updatedAt: "",
+        category: 2,
+        title: "안녕하세요33",
+        viewNum: 12,
+        content: "하이하이33",
+        author: 1,
+        thumbnail: 3
+    },
+    {
+        id: 4,
+        createdAt: "",
+        updatedAt: "",
+        category: 3,
+        title: "안녕하세요44",
+        viewNum: 12,
+        content: "하이하이44",
+        author: 1,
+        thumbnail: 3
+    }
+]
